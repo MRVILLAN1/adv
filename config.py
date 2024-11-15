7375a5da4bbf34cb83eb60a37790fde5b8a67ed2
MRVILLAN1 <157270721+MRVILLAN1@users.noreply.github.com>
2024-11-16 01:17:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "10000844"))
-API_HASH = getenv("API_HASH", "776f257fc1d1f8aa4aea9dd35d10a45b")
-BOT_TOKEN = getenv("BOT_TOKEN", "6743648885:AAFGGf9BjIiXf-dTKWss2ocn-UgkUIZOGJY")
-OWNER_ID = int(getenv("OWNER_ID", "6750546542"))
+API_ID = int(getenv("API_ID", "17995671"))
+API_HASH = getenv("API_HASH", "37adac03f1c113a3126b1f9eea32aff4")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = int(getenv("OWNER_ID", "7464865613"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://fiona171593:tbGMvepmKQ8YNfJy@cluster0.5ccbrkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-LOG_GROUP = int(getenv("LOG_GROUP", "-1004530635818"))
-FORCESUB = getenv("FORCESUB", "funnyzilla")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002499315195"))
+FORCESUB = getenv("FORCESUB", "")
